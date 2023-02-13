29e849fd341f20e9f6054b8ad27e674a04538654
Kevin Salinas <kevinsalinas155@gmail.com>
2023-02-13 12:45:27 -0500

    created filter() function for Genre

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 fetch("  http://localhost:3000/games")
     .then(res => res.json())
     .then(gamesArray => {
+        filterGenre(gamesArray),                //<--- HUGE WIN ON THIS
         gamesArray.forEach( gameObj => {
             renderGame( gameObj )
         })
     })
-
 
 const gameCollection = document.querySelector("#game-collection")
 
@@ -14,7 +14,13 @@ function renderGame(game) {
     const img = document.createElement('img')
     img.src = game.thumbnail
     gameCollection.append(img)
-    
-    
 
+}
+
+
+function filterGenre(gamesArray) {
+    gamesArray.filter( gameGenre => {
+         if ( gameGenre.genre == "Shooter")
+             return console.log(gameGenre)
+     } )
 }
