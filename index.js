f6e70dc4da2adb1cdc9fee38f228ab68c15847a8
Dakota Vikdal <dakota.james19@hotmail.com>
2023-02-13 15:56:16 -0500

    Few Css changes and javascript description/title

--- index.js
+++ index.js
@@ -2,18 +2,30 @@
 fetch("  http://localhost:3000/games")
     .then(res => res.json())
     .then(gamesArray => {
-        filterGenre(gamesArray),                //<--- HUGE WIN ON THIS
+        filterGenre(gamesArray),
         gamesArray.forEach( gameObj => {
             renderGame( gameObj )
         })
     })
 
 const gameCollection = document.querySelector("#game-collection")
+const allGamesBtn = document.querySelector("#all-games")
+
+const shooterBtn = document.querySelector("#shooter")
+const mmoarpgBtn = document.querySelector('#mmoarpg')
+const strategyBtn = document.querySelector('#Strategy')
+
+
+
 
 function renderGame(game) {
     const img = document.createElement('img')
+    const title = document.createElement('h1')
+    const description = document.createElement('p')
     img.src = game.thumbnail
-    gameCollection.append(img)
+    title.textContent = game.title
+    description.textContent = game.short_description
+    gameCollection.append(img, title, description)
 
 }
 
@@ -23,4 +35,7 @@ function filterGenre(gamesArray) {
          if ( gameGenre.genre == "Shooter")
              return console.log(gameGenre)
      } )
+     
 }
+shooterBtn.addEventListener('click', filterGenre)
+console.log(shooterBtn);
